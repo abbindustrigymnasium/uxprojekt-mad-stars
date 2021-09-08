f39dc3062c74a8d71abf0f4ce69ebfc7e7828ba3
AbbMyBjo <54663051+AbbMyBjo@users.noreply.github.com>
2021-09-08 09:16:18 +0200

    .

--- Frontend/src/layouts/MainLayout.vue
+++ Frontend/src/layouts/MainLayout.vue
@@ -2,42 +2,9 @@
   <q-layout class="background">
     <q-footer class="bg-color">
         <q-toolbar class="toolbar">
-          <!-- <div class="q-pa-md q-gutter-sm">
-            <q-btn align="left" class="btn-fixed-width" color="primary" label="Align to left" />
-            <q-btn align="right" class="btn-fixed-width" color="secondary" label="Align to right" />
-            <q-btn align="between" class="btn-fixed-width" color="accent" label="Align between" icon="flight_takeoff" />           
-          </div> -->
-         
           <div class="q-pa-md q-gutter-sm">          
             <q-btn class="full-width" flat style="color: #000000" label="Tap to order" to="/meny" />            
           </div>
-          
-
-           <!-- <div class="q-pa-md doc-container">
-            <div class="row items-start"> 
-              <div class="col">
-                <q-btn 
-                    class="cancel"
-                    label="Cancel"
-                    to="/"
-                />
-              </div>
-              <div class="col">
-                <q-btn 
-                    class="cart"
-                    icon="shopping_cart"
-                    to="/cart"
-                />
-              </div>
-              <div class="col">
-                <q-btn 
-                    class="check-out"
-                    label="To Check-Out"
-                    to="/check-out"
-                />
-              </div>
-            </div>
-          </div> -->
         </q-toolbar>
       </q-footer>
   </q-layout>
